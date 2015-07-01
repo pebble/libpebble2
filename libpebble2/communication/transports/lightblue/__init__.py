ff1ef2be47f54280a51018846c8c954fcf77f05b
Katharine Berry <katharine@getpebble.com>
2015-07-01 15:02:42 -0700

    lightblue.

---
+++ libpebble2/communication/transports/lightblue/__init__.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+import logging
+import multiprocessing
+import os
+import Queue
+import re
+import socket
+from multiprocessing import Process
+from struct import unpack
+
+from .. import BaseTransport, MessageTargetWatch
+
+logger = logging.getLogger("libpebble2.communication.transports.lightblue")
+
+
+class LightBluePebbleError(Exception):
+    def __init__(self, id, message):
+        self._id = id
+        self._message = message
+
+    def __str__(self):
+        return "%s ID:(%s) on LightBlue API" % (self._message, self._id)
+
+
+class LightBlueTransport(BaseTransport):
+    must_initialise = True
+
+    def __init__(self, pebble_id, should_pair, debug_protocol=False, connection_process_timeout=60):
+        self.connection_process_timeout = connection_process_timeout
+        self.mac_address = pebble_id
+        self.debug_protocol = debug_protocol
+        self.should_pair = should_pair
+
+        manager = multiprocessing.Manager()
+        self.send_queue = manager.Queue()
+        self.rec_queue = manager.Queue()
+
+        self.bt_teardown = multiprocessing.Event()
+        self.bt_message_sent = multiprocessing.Event()
+        self.bt_connected = multiprocessing.Event()
+
+        self.bt_socket_proc = Process(target=self._run)
+        self.bt_socket_proc.daemon = True
+
+        self._connected = False
+
+    def send_packet(self, message, target=MessageTargetWatch()):
+        assert isinstance(target, MessageTargetWatch)
+        try:
+            self.send_queue.put(message)
+            self.bt_message_sent.wait()
+        except:
+            self.bt_teardown.set()
+            logger.debug("LightBlue process has shut down (queue write)")
+            raise
+
+    def connected(self):
+        return self._connected
+
+    def connect(self):
+        logger.info("Starting lightblue proccess.")
+        self.bt_socket_proc.start()
+        self.bt_socket_proc.join()
+        print self.bt_socket_proc.exitcode
+
+        # wait for a successful connection from child process before returning to main process
+        logger.info("Waiting...")
+        self.bt_connected.wait(self.connection_process_timeout)
+        if not self.bt_connected.is_set():
+            raise LightBluePebbleError(self.mac_address,
+                                       "Connection timed out, LightBlueProcess was provided {} seconds to complete "
+                                       "connecting" % self.connection_process_timeout)
+        self._connected = True
+
+    def _run(self):
+        logger.info("LightBlue process has started on pid %d" % os.getpid())
+        # These are imported late to avoid the overhead when not using lightblue.
+        from lightblue import pair, socket as lb_socket, finddevices, selectdevice
+        try:
+            import sys
+            print 'hi'
+        except ImportError:
+            logger.error("Import failed.")
+        else:
+            print "import okay."
+        import sys
+        logger.debug('imported.')
+        print 'o.o'
+
+        def autodetect(self):
+            list_of_pebbles = list()
+
+            if self.mac_address is not None and len(self.mac_address) is 4:
+                # we have the friendly name, let's get the full mac address
+                logger.info("Going to get full address for device %s, ensure device is broadcasting." % self.mac_address)
+                # scan for active devices
+                devices = finddevices(timeout=8)
+
+                for device in devices:
+                    if re.search(r'Pebble ' + self.mac_address, device[1], re.IGNORECASE):
+                        logger.debug("Found Pebble: %s @ %s" % (device[1], device[0]))
+                        list_of_pebbles.append(device)
+
+                if len(list_of_pebbles) is 1:
+                    return list_of_pebbles[0][0]
+                else:
+                    raise LightBluePebbleError(self.mac_address, "Failed to find Pebble")
+            else:
+                # no pebble id was provided... give them the GUI selector
+                try:
+                    return selectdevice()[0]
+                except TypeError:
+                    logger.warn("failed to select a device in GUI selector")
+                    self.mac_address = None
+
+        # notify that the process has started
+        logger.debug("Starting...")
+        print "help"
+
+        # do we need to autodetect?
+        if self.mac_address is None or len(self.mac_address) is 4:
+            print "autodetecting"
+            self.mac_address = autodetect(self)
+        print '???'
+        # create the bluetooth socket from the mac address
+        if self.should_pair and self.mac_address is not None:
+            print "pairing"
+            pair(self.mac_address)
+            print "paired"
+        print '!!!'
+        try:
+            print "creating socket"
+            self._bts = lb_socket()
+            print "connecting to {}".format(self.mac_address)
+            self._bts.connect((self.mac_address, 1))  # pebble uses RFCOMM port 1
+            print "nonblocking"
+            self._bts.setblocking(False)
+        except:
+            raise LightBluePebbleError(self.mac_address, "Failed to connect to Pebble")
+
+        # give them the mac address for using in faster connections
+        logger.debug("Connection established to " + self.mac_address)
+
+        # Tell our parent that we have a pebble connected now
+        self.bt_connected.set()
+
+        send_data = e = None
+        while not self.bt_teardown.is_set():
+            # send anything in the send queue
+            try:
+                send_data = self.send_queue.get_nowait()
+                self._bts.send(send_data)
+                logger.debug("LightBlue Send: %r", send_data)
+                self.bt_message_sent.set()
+            except Queue.Empty:
+                pass
+            except (IOError, EOFError):
+                self.bt_teardown.set()
+                e = "Queue Error while sending data"
+
+            # if anything is received relay it back
+            rec_data = None
+            try:
+                rec_data = self._bts.recv(4)
+            except (socket.timeout, socket.error):
+                # Exception raised from timing out on nonblocking
+                pass
+
+            if rec_data is not None and len(rec_data) == 4:
+                # check the Stream Multiplexing Layer message and get the length of the data to read
+                size, endpoint = unpack("!HH", rec_data)
+                resp = ''
+                while len(resp) < size:
+                    try:
+                        resp += self._bts.recv(size - len(resp))
+                    except (socket.timeout, socket.error):
+                        # Exception raised from timing out on nonblocking
+                        # TODO: Should probably have some kind of timeout here
+                        pass
+                try:
+                    resp = rec_data + resp
+                    self.rec_queue.put(resp)
+
+                except (IOError, EOFError):
+                    self.bt_teardown.set()
+                    e = "Queue Error while recieving data"
+                    pass
+                logger.debug("LightBlue Read: %r " % resp)
+
+        # just let it die silent whenever the parent dies and it throws an EOFERROR
+        if e is not None and self.debug_protocol:
+            raise LightBluePebbleError(self.mac_address, "LightBlue polling loop closed due to " + e)
+
+    def read_packet(self):
+        try:
+            while True:
+                try:
+                    message = self.rec_queue.get()
+                    return MessageTargetWatch(), message
+                except Queue.Empty:
+                    continue
+        except:
+            self.bt_teardown.set()
+            if self.debug_protocol:
+                logger.debug("LightBlue process has shutdown (queue read)")
+            raise
