3ece8300c876cf8c9d38b73a462b5dd0378af386
Katharine Berry <katharine@getpebble.com>
2015-07-04 03:01:49 -0700

    Clean up for publishing.

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ __author__ = 'katharine'
 from setuptools import setup, find_packages
 
 setup(name='libpebble2',
-      version='0.0.0',
+      version='0.0.1',
       description='Library for communicating with pebbles over pebble protocol',
-      url='https://github.com/pebble/kb-libpebble2',
+      url='https://github.com/pebble/libpebble2',
       author='Pebble Technology Corporation',
       author_email='katharine@pebble.com',
       license='MIT',
@@ -13,5 +13,19 @@ setup(name='libpebble2',
       install_requires=[
         'enum34>=1.0.4',
         'websocket-client>=0.31.0',
+        'six>=1.9.0',
+      ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Embedded Systems',
       ],
       zip_safe=True)
