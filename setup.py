d529a377e616fab0d23c43ad715e58ab748f6772
Katharine Berry <katharine@getpebble.com>
2015-07-08 01:02:16 -0700

    Enable travis-ci.

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 __author__ = 'katharine'
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+import sys
+
+
+class PyTest(TestCommand):
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here because outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(["-v"])
+        sys.exit(errno)
 
 setup(name='libpebble2',
       version='0.0.1',
@@ -15,6 +35,10 @@ setup(name='libpebble2',
         'websocket-client>=0.31.0',
         'six>=1.9.0',
       ],
+      tests_require=[
+        'pytest'
+      ],
+      cmdclass={'test': PyTest},
       classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
